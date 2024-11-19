849eefe4dca0b86d3869f729524d116392010eea
Carlicsus <carlicsus3@gmail.com>
2024-11-19 13:39:09 -0600

    Se da funcionalidad a los botones del menu

--- Menu.py
+++ Menu.py
@@ -7,6 +7,8 @@ class Menu(QtWidgets.QMainWindow, Ui_FrmMenu):
         self.setupUi(self)
         self.pushButton.clicked.connect(self.openEncript)
 
+        self.menuEncriptar.addAction("Encriptar", self.openEncript)
+
     def openEncript(self):
         openNewWindow = Limpiar(self)
         openNewWindow.show()
