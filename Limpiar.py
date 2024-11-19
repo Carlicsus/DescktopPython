1478f729411ad002fbf909ef70a6aefeb7f9717b
Carlicsus <carlicsus3@gmail.com>
2024-11-19 14:14:01 -0600

    Se encripta el mensaje y se da funcionalidad al boton de limpiar

--- Limpiar.py
+++ Limpiar.py
@@ -1,5 +1,7 @@
 from FrmLimpiar import *
 from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
 
 class Limpiar(QtWidgets.QMainWindow, Ui_FrmLimpiar):
     def __init__(self, *args, **kwargs):
@@ -7,6 +9,7 @@ class Limpiar(QtWidgets.QMainWindow, Ui_FrmLimpiar):
         self.setupUi(self)
         
         self.btn_encriptar.clicked.connect(self.encriptarAES)
+        self.btn_limpiar.clicked.connect(self.limpiar)
         
     def encriptarAES(self):
         data= self.txt_mensaje.toPlainText()
@@ -15,4 +18,12 @@ class Limpiar(QtWidgets.QMainWindow, Ui_FrmLimpiar):
         iv = b"TI_UTXJ2024ENCRI"
         
         padder= padding.PKCS7(128).padder()
-        print(padder)
+        padded_data = padder.update(data.encode('utf-8'))
+        padded_data += padder.finalize()
+        cipher = Cipher(algorithms.AES(key),modes.CBC(iv), backend = default_backend())
+        encryptor = cipher.encryptor()
+        ciphertext = encryptor.update(padded_data)+encryptor.finalize()
+        self.label_4.setText(f'${ciphertext}')
+        
+    def limpiar(self):
+        self.label_4.clear()
